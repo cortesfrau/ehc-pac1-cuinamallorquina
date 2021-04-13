d5ab06d90f06b28ed13ba498d02cb07f8c224d10
Lluís Cortès <lluiscortes@outlook.com>
2021-04-13 23:12:05 +0200

    Commit

---
+++ imagemin.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "gifsicle": { "optimizationLevel": 2,
+    "interlaced": false,
+    "colors": 10 },
+  "mozjpeg": {
+    "progressive": true,
+    "quality": 1
+  },
+  "pngquant": {
+    "quality": [0.25, 0.5]
+  },
+  "svgo": {
+    "plugins": [
+      { "removeViewBox": false },
+      { "cleanupIDs": true },
+    ]
+  },
+  "webp": {
+    "quality": 1
+  }
+}
