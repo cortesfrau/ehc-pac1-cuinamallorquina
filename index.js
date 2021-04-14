6fbb1bd6dad92650cb80f2cc713169ed328900ef
Lluís Cortès <lluiscortes@outlook.com>
2021-04-14 16:55:35 +0200

    Commit

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ window.onload = function(){
     document.body.style.position = 'fixed';
   }
 
+  // Active header links
+  var current = location.pathname.split('/')[1];
+  if (current === "") return;
+  var menuItems = document.querySelectorAll('.menu li a');
+  for (var i = 0, len = menuItems.length; i < len; i++) {
+    if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
+      menuItems[i].className += "active";
+    }
+  }
+
 }
